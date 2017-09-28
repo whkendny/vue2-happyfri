cb91c216bb513d96f74576795c70607f67a011b1
DESKTOP-9MOB\wedo <wh_kendny@163.com>
2017-09-28 12:46:09 +0800

    add

--- src/main.js
+++ src/main.js
@@ -17,11 +17,15 @@ import ajax from './config/ajax'
 import './style/common'
 import './config/rem'
 
+//在一个模块化工程中使用它，必须要通过 Vue.use() 明确地安装路由功能：
 Vue.use(VueRouter)
+//创建 router 实例，然后传 `routes` 配置
 const router = new VueRouter({
 	routes
 })
-
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
 new Vue({
 	router,
 	store,
--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,7 @@ import App from '../App'
 export default [{
     path: '/',
     component: App,
+    // 子路由配置, 将组件(components)映射到路由(routes)，然后告诉 vue-router 在哪里渲染它们。
     children: [{
         path: '',
         component: r => require.ensure([], () => r(require('../page/home')), 'home')
